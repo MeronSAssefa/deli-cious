a7302eb2a7a9c96bfbb98909b0246cef553da72b
MeronSAssefa <meronseife6@gmail.com>
2026-05-26 13:29:00 -0700

    created a Drink object

--- src/main/java/com/pluralsight/model/Drink.java
+++ src/main/java/com/pluralsight/model/Drink.java
@@ -1,4 +1,36 @@
 package com.pluralsight.model;
 
 public class Drink {
+
+    private String Size;
+    private String flavor;
+    private double price;
+
+    public Drink(String size, String flavor) {
+        this.Size = size;
+        this.flavor = flavor;
+
+        if (size.equalsIgnoreCase("small")) {
+            this.price = 2.00;
+        } else if (size.equalsIgnoreCase("medium")) {
+            this.price = 2.50;
+        } else if (size.equalsIgnoreCase("large")) {
+            this.price = 3.00;
+        } else {
+            this.price = 0.00;
+        }
+
+    }
+
+    public String getSize() {
+        return Size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,6 +1,7 @@
 package com.pluralsight.ui;
 
 import com.pluralsight.model.Chips;
+import com.pluralsight.model.Drink;
 import com.pluralsight.model.Order;
 
 import java.util.Scanner;
@@ -67,6 +68,7 @@ public class UserInterface {
                     break;
                 case "2":
                     System.out.println("Adding Drinks");
+                    addDrinkToOrder(currentOrder);
                     break;
                 case "3":
                     System.out.println("Adding Chips");
@@ -101,6 +103,25 @@ public class UserInterface {
         currnetOrder.addChip(chips);
 
         System.out.println(chipsChoice + " Chips added to your order");
+    }
+
+    public void addDrinkToOrder(Order currentOrder) {
+
+        System.out.println("What size drink would you like?");
+        System.out.println("Small");
+        System.out.println("Medium");
+        System.out.println("Large");
+        System.out.print("Enter size: ");
+        String size = scanner.nextLine();
+
+        System.out.print("What flavor drink would you like? ");
+        String flavor = scanner.nextLine();
+
+        Drink drink = new Drink(size, flavor);
+
+        currentOrder.addDrink(drink);
+
+        System.out.println(size + " " + flavor + " drink added to your order.");
     }
 
 }
